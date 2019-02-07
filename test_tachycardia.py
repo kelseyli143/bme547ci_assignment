ee561c8e9d4a0370a2bc0f59dc5ea9f49de29932
Kelsey Li <kelseyli@Kelseys-MacBook-Pro-2.local>
2019-02-07 01:49:40 -0500

    tried changing indentation in pytest parametrize decorator for pep8 standards

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -3,11 +3,11 @@
 import pytest
 
 
-@pytest.mark.parametrize(
-    "a, expected", [('tachycardic', True),
-    ('TachycarDic', True), ('....,.,.   tachycardic   ', True),
-    ('TACHYCARDIC', True), ('kelsey', False), ('no heart issues', False)]
-    )
+@pytest.mark.parametrize("a, expected", [
+                         ('tachycardic', True),
+                         ('TachycarDic', True), ('....,.,.   tachycardic   ', True),
+                         ('TACHYCARDIC', True), ('kelsey', False), ('no heart issues', False)
+                         ])
 def test_is_tachycardic_param(a, expected):
     from tachycardia import is_tachycardic
     answer = is_tachycardic(a)
